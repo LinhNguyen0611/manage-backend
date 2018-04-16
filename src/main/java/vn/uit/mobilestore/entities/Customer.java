f0fcfe4199ae7be11d59f23c9cbb8cdb771a4581
LinhNguyen0611 <annienguyen.it@gmail.com>
2018-04-16 23:46:15 +0700

    WIP: Order

---
+++ src/main/java/vn/uit/mobilestore/entities/Customer.java
@@ -0,0 +1,94 @@
+package vn.uit.mobilestore.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Linh Nguyen on 4/13/2018.
+ */
+@Entity
+@Table(name = "Customer")
+public class Customer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CustomerID", nullable = false)
+    private Integer customerID;
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @Column(name = "Phone")
+    private String phone;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "Birthday")
+    private Date birthday;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders;
+
+    public Customer() {
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
