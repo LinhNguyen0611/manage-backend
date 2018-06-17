665247adf86660809889f971b792c3a446a59f2f
trungnguyen27 <quoctrunguit@gmail.com>
2018-06-17 11:22:33 +0700

    Add list-by-id non auth

--- src/main/java/vn/uit/mobilestore/config/WebSecurity.java
+++ src/main/java/vn/uit/mobilestore/config/WebSecurity.java
@@ -44,7 +44,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .antMatchers("/item/get/**", "/model/get/**", "/brand/get/**", "/variant/get/**",
                 "/supplier/get/**", "/stockReceivingOrder/get/**", "/stockReceivingItem/get/**").permitAll()
-                .antMatchers("/item/list/**", "/model/list/**", "/brand/list/**", "/variant/list/**",
+                .antMatchers("/item/list/**", "/model/list/**", "/brand/list/**", "/brand/list-by-id/**" , "/variant/list/**",
                         "/supplier/list/**", "/stockReceivingOrder/list/**", "/stockReceivingItem/list/**").permitAll()
                 // Enable swagger
                 .antMatchers("/v2/api-docs", "/configuration/**", "/swagger-resources/**",
